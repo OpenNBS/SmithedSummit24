1815eb18555000238ddcd3eee3e9f2a75eb07a5b
Bentroen <29354120+Bentroen@users.noreply.github.com>
2024-11-06 13:33:35 -0300

    fix: re-implement models from original pack selectively

--- src/panel/panel.py
+++ src/panel/panel.py
@@ -1,5 +1,39 @@
-from beet import Context
+import json
+from pathlib import Path
+
+from beet import Context, Texture
+from PIL import Image
+
+from src.model import apply_emissive_textures, generate_model_predicates
+
+models = ["nbs:nbw_block", "nbs:nbw_text"]
+textures = [
+    "nbs:block/nbw_left",
+    "nbs:block/nbw_right",
+    "nbs:block/nbw_top",
+    "nbs:block/nbw_text",
+]
 
 
 def beet_default(ctx: Context):
-    pass
+    textures_to_delete = []
+    for texture in ctx.assets.textures:
+        if texture not in textures:
+            textures_to_delete.append(texture)
+    for texture in textures_to_delete:
+        del ctx.assets.textures[texture]
+        if texture in ctx.assets.textures_mcmeta:
+            del ctx.assets.textures_mcmeta[texture]
+
+    models_to_delete = []
+    for model in ctx.assets.models:
+        if model not in models:
+            models_to_delete.append(model)
+    for model in models_to_delete:
+        del ctx.assets.models[model]
+
+    ctx.assets["minecraft:item/note_block"] = generate_model_predicates(
+        "block/note_block", [models.replace("nbs:", "") for models in models]
+    )
+
+    apply_emissive_textures(ctx)
